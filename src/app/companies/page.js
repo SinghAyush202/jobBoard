22edb6136e54ebdc566b8def76cef3879f30935e
Ayush Singh <singhayush00314@gmail.com>
2024-08-27 01:23:19 +0530

    feat: fixes all bugs

--- src/app/companies/page.js
+++ src/app/companies/page.js
@@ -1,78 +1,16 @@
- "use client";
+import { fetchJobsForCandidateAction, fetchProfileAction } from "../../action/index";
+import Companies from "../../components/companies";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-import { useRouter } from "next/navigation";
-import CommonCard from "../../components/common-card";
-import JobIcon from "../../components/job-icon/index";
-import { Button } from "../../components/ui/button";
+async function CompaniesPage() {
+  const user = await currentUser();
+  const profileInfo = await fetchProfileAction(user?.id);
 
-function Companies({ jobsList }) {
-    console.log(jobsList)
-  const router = useRouter();
+  if (!profileInfo) redirect("/onboard");
+  const jobsList = await fetchJobsForCandidateAction({});
 
-  const createUniqueSetOfCompanies = [
-    ...new Set(
-      jobsList
-        .filter(
-          (jobItem) =>
-            jobItem?.companyName && jobItem?.companyName.trim() !== ""
-        )
-        .map((item) => item.companyName)
-    ),
-  ];
-
-  function handleFilterJobsByCompanyName(getCompanyName) {
-    sessionStorage.setItem(
-      "filterParams",
-      JSON.stringify({
-        companyName: [getCompanyName],
-      })
-    );
-
-    router.push("/jobs");
-  }
-
-  console.log(createUniqueSetOfCompanies);
-
-  return (
-    <div className="mx-auto max-w-7xl">
-      <div className="flex items-baseline dark:border-white justify-between border-b pb-6 pt-24">
-        <h1 className="text-4xl dark:text-white font-bold tracking-tight text-gray-900">
-          Browse Companies
-        </h1>
-      </div>
-      <div className="pt-6 pb-24">
-        <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-3">
-          <div className="lg:col-span-4">
-            <div className="container mx-auto p-0 space-y-8">
-              <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-3">
-                {createUniqueSetOfCompanies &&
-                createUniqueSetOfCompanies.length > 0 ? (
-                  createUniqueSetOfCompanies.map((companyName) => (
-                    <CommonCard
-                      icon={<JobIcon />}
-                      title={companyName}
-                      footerContent={
-                        <Button
-                          onClick={() =>
-                            handleFilterJobsByCompanyName(companyName)
-                          }
-                          className="dark:bg-[#fffa27] h-11 flex items-center justify-center px-5"
-                        >
-                          See Jobs
-                        </Button>
-                      }
-                    />
-                  ))
-                ) : (
-                  <h1>No Companies present!</h1>
-                )}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <Companies jobsList={jobsList} />;
 }
 
-export default Companies;
+export default CompaniesPage;
