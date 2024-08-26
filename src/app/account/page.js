520706b97e1befb1514ff9a2dca6290743303dc6
Ayush Singh <singhayush00314@gmail.com>
2024-08-27 00:08:57 +0530

    feat: failing right now

---
+++ src/app/account/page.js
@@ -0,0 +1,12 @@
+import { fetchProfileAction } from "@/actions";
+import AccountInfo from "@/components/account-info";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default async function AccountPage() {
+  const user = await currentUser();
+  console.log("sdlfjsd")
+  const profileInfo = await fetchProfileAction(user?.id);
+  if (!profileInfo) redirect("/onboard");
+  return <AccountInfo profileInfo={profileInfo} />;
+}
